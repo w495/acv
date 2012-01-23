8483a249f9d9753a3063d6355c5a3b9a633bd2f4
w-495 <w@w-495.ru>
2012-01-23 14:30:26 +0400

    update mochiweb

--- web/priv/sql/create/insert.sql
+++ web/priv/sql/create/insert.sql
@@ -1,5 +1,5 @@
 /**
- * Сustomers
+ * \file Вставка данных в базу
  *
 **/
 
@@ -43,111 +43,7 @@ insert into customer2group (customer_id, group_id)
 		((select id from customer where login='admin'),
 			(select id from customer_group where name='all'));
 
-/**
- * DOCS
- *
-**/
 
-insert into doc_type (name)
-	values
-        ('descr'),
-        ('doc'),
-        ('conf_answ');
-
-insert into attach_type (name)
-	values
-		('file'),
-		('video'),
-		('image'),
-		('link');
-
-insert into dir_type (name)
-	values ('news'),
-		('subdir'),
-		('key'),
-		('root_test'),
-			('root_test_surface'),
-			('root_test_air'),
-			('root_test_water'),
-		('test'),
-			('test_question'),
-        ('root_conference'),
-            ('conference'),
-                ('conf_question'),
-                ('conf_answ'),
-		('term'),
-			('term_air'),
-			('term_surface'),
-			('term_water');
-
-/**
-	Вызываем хранимку для создания корней.
-	Базовые папки прошитые жестко.
-**/
-
-/* ПАПКИ ПЕРВОГО УРОВНЯ */
-
-select create_root_dir('/', 						    'key');
-select create_root_dir('Новости', 						'news');
-select create_root_dir('Нормативные акты', 				'subdir');
-select create_root_dir('Безопасность на транспорте', 	'subdir');
-select create_root_dir('Конференции', 					'root_conference');
-select create_root_dir('Тесты', 						'root_test');
-select create_root_dir('Термины', 						'term');
-
-/* ПАПКИ ВТОРОГО УРОВНЯ: Термины */
-
-select create_hard_dir('Воздушные термины', 'Термины');
-select create_hard_dir('Водные термины', 	'Термины');
-select create_hard_dir('Наземные термины',  'Термины');
-
-/* ПАПКИ ВТОРОГО УРОВНЯ: Норматиыные акты */
-
-select create_hard_dir('Головная страница',  					'/');
-select create_hard_dir('Воздушный трансаорт',  					'/');
-select create_hard_dir('Водный трансаорт',  					'/');
-select create_hard_dir('Наземный трансаорт',  					'/');
-
-
-/* ПАПКИ ВТОРОГО УРОВНЯ: Тесты */
-
-select create_hard_dir_by_name('Воздушные тесты',  	'root_test_air',		'Тесты');
-select create_hard_dir_by_name('Водные тесты',  	'root_test_water',		'Тесты');
-select create_hard_dir_by_name('Наземные тесты',  	'root_test_surface',	'Тесты');
-
-
-
-select create_hard_dir('Нормативно-правовые документы',  					'Нормативные акты');
-select create_hard_dir('Указы президентa РФ',  			 					'Нормативные акты');
-select create_hard_dir('Постановления правительства РФ', 					'Нормативные акты');
-select create_hard_dir('Нормативно-правовые акты министерства',  			'Нормативные акты');
-select create_hard_dir('Мониторинг законодательства',  						'Нормативные акты');
-select create_hard_dir('Экспертиза проектов документов',  					'Нормативные акты');
-select create_hard_dir('Нормативно-правовые акты министерств и ведомств',  	'Нормативные акты');
-select create_hard_dir('Федеральные конституционные законы',  				'Нормативные акты');
-select create_hard_dir('Докуметы МЧС в системе "Гарант"',  					'Нормативные акты');
-select create_hard_dir('Порядок обжалования нормативных правовых актов',  	'Нормативные акты');
-select create_hard_dir('Мероприятия проводимые по реализации ФЗ №83',  		'Нормативные акты');
-
-
-select create_hard_dir('Военные и транспорт',  									'Безопасность на транспорте');
-select create_hard_dir('Транспорт и экология',  								'Безопасность на транспорте');
-select create_hard_dir('Протекционизм и таможни',  								'Безопасность на транспорте');
-select create_hard_dir('Организация движения',  								'Безопасность на транспорте');
-select create_hard_dir('Организация транспортного регулирования',  				'Безопасность на транспорте');
-select create_hard_dir('Либерализация транспорта',  							'Безопасность на транспорте');
-select create_hard_dir('Экономическая теория о транспортном регулировании',  	'Безопасность на транспорте');
-select create_hard_dir('Транспортная инфраструктура',  							'Безопасность на транспорте');
-select create_hard_dir('Пространство движения',  								'Безопасность на транспорте');
-select create_hard_dir('Перспективные транспортные технологии',  				'Безопасность на транспорте');
-select create_hard_dir('Человеческий фактор в безопасности транспорта',  		'Безопасность на транспорте');
-
-
-/* ЛОГИРОВАНИЕ */
-
-insert into log_object_type (name, alias) values ('directory', 'директория'), ('document', 'документ'), ('test', 'тест'), ('test_answer', 'ответ теста'), ('attach', 'аттач');
-insert into log_action_type (name) values ('create'), ('delete'), ('update');
-
-/* БАННЕРЫ */
 
 insert into banner_place (name, alias) values ('right', 'справа'), ('bottom', 'снизу');
+
--- web/priv/sql/create/scheme.sql
+++ web/priv/sql/create/scheme.sql
@@ -1,3 +1,65 @@
+/**
+ * \file Описание схемы базы данных
+**/
+
+create sequence seq_banner_place;
+create table banner_place (
+    id int primary key default nextval('seq_banner_place'),
+    name varchar(30) unique,
+    alias varchar(100)
+);
+
+
+create sequence seq_acv_banner;
+create table acv_banner (
+    id int primary key default nextval('seq_acv_banner'),
+    name varchar(100),
+    datestart timestamp without time zone,
+    datestop timestamp without time zone,
+    banner_place_id int references banner_place(id),
+    url varchar(200),
+    ref varchar(1000)
+);
+
+create sequence seq_acv_video;
+create table acv_video (
+    id int primary key default nextval('seq_acv_video'),
+    name varchar(100),
+    datestart timestamp without time zone,
+    datestop timestamp without time zone,
+    url varchar(200),
+    ref varchar(1000),
+    wish int,
+    postroll bool default true,
+    preroll bool default true,
+    midroll bool default true,
+    pauseroll bool default true,
+    user_male bool default null,
+    age_from int default null,
+    age_to int default null
+);
+
+create sequence seq_geo_region;
+create table geo_region (
+    id int primary key default nextval('seq_geo_region'),
+    alias varchar(30) UNIQUE,
+    name varchar (100)
+);
+
+create sequence seq_tgv_region;
+create table tgv_region (
+    id int primary key default nextval('seq_tg_region'),
+    ac_video_id int references acv_video(id),
+    geo_region_id int references geo_region(id)
+);
+
+create sequence seq_tgv_category;
+create table tgv_category (
+    id int primary key default nextval('seq_tgv_category'),
+    ac_video_id int references acv_video(id),
+    cat_id int
+);
+
 create sequence seq_customer_id;
 create table customer(
     id int primary key default nextval('seq_customer_id'),
@@ -49,168 +111,4 @@ create table customer2group (
     group_id int references customer_group(id) not null
 );
 
-create sequence seq_doc_type;
-create table doc_type(
-    id int primary key default nextval('seq_doc_type'),
-    name varchar(1024)
-);
 
-create sequence seq_document;
-create table document (
-    id int primary key default nextval('seq_document'),
-    name varchar(1024),
-    content text,
-    datatime timestamp without time zone default now(),
-    published bool,
-    updater int,
-    pic_url text, /* заглавная картинка для новости */
-    pdf_url text,
-    msw_url text,
-    dir_id int,
-    doc_type_id int references doc_type(id)
-);
-
-
-create sequence seq_dir_type;
-create table dir_type (
-    id int primary key default nextval('seq_dir_type'),
-    name varchar(1024)
-);
-
-create sequence seq_directory;
-create table directory (
-    id int primary key default nextval('seq_directory'),
-    name varchar(1024),
-    parent_dir_id int references directory(id),
-    datatime timestamp without time zone default now(),
-    updater int,
-    doc_description_id int references document(id),
-    dir_type_id int references dir_type(id),
-    deleted bool
-);
-
-alter table document add constraint document_dir_id_fkey
-	foreign key (dir_id) references directory(id);
-
-create sequence seq_attach_type;
-create table attach_type (
-    id int primary key default nextval('seq_attach_type'),
-    name varchar(1024)
-);
-
-create sequence seq_attach2doc;
-create table attach2doc (
-    id int primary key default nextval('seq_attach2doc'),
-    name varchar(1024),
-    alt text,
-    doc_id int references document(id),
-    attach_type_id int references attach_type(id),
-    url text,
-    updater int
-);
-
-
-/* ТЕСТЫ */
-
-create sequence seq_test2customer;
-create table test2customer (
-    id int primary key default nextval('seq_test2customer'),
-    customer_id int references customer(id),
-    test_id int references directory(id),
-    result int,
-    datetime timestamp without time zone default now()
-);
-
-create table quest_answer (
-    answer_id int references document(id),
-    correct_flag bool
-);
-
-/* КОНФЕРНЕЦИИ */
-
-create table conference (
-    conf_id int references directory(id),
-    start date,
-    stop date
-);
-
-create table conf_quest_subscriber (
-    conf_quest_id int references directory(id),
-    customer_id int references customer(id)
-);
-
-
-/* ЛОГИРОВАНИЕ */
-
-create sequence seq_log_object_type;
-create table log_object_type (
-    id int primary key default nextval('seq_log_object_type'),
-    name varchar(50) unique,
-    alias varchar(50)
-);
-
-
-create sequence seq_log;
-create table log (
-    id int primary key default nextval('seq_log'),
-    log_object_id int,
-    log_object_type_id int references log_object_type(id),
-    datetime timestamp without time zone default now(),
-    customer_id int references customer(id)
-);
-
-create sequence seq_log_action_type;
-create table log_action_type (
-    id int primary key default nextval('seq_log_object_type'),
-    name varchar(50) unique
-);
-
-
-
-create table log_action (
-    log_id int references log(id),
-    log_action_type_id int references log_action_type(id),
-    field_name varchar(50),
-    old_value text,
-    new_value text
-);
-
-
-/* БАННЕРЫ */
-
-create sequence seq_banner_place;
-create table banner_place (
-    id int primary key default nextval('seq_banner_place'),
-    name varchar(30) unique,
-    alias varchar(100)
-);
-
-
-create sequence seq_adv_com;
-create table adv_com (
-    id int primary key default nextval('seq_adv_com'),
-    name varchar(1024),
-    datestart timestamp without time zone,
-    datestop timestamp without time zone,
-    banner_place_id int references banner_place(id),
-    url varchar(200),
-    ref varchar(1000)
-);
-
-
-create sequence seq_inf_src;
-create table inf_src (
-    id int primary key default nextval('seq_inf_src'),
-    name varchar(1024),
-    update_datetime timestamp without time zone,
-    regexp text,
-    value text,
-    url varchar(200)
-);
-
-create sequence seq_game_map;
-create table game_map (
-    id int primary key default nextval('seq_game_map'),
-    name varchar(1024),
-    data text
-);
