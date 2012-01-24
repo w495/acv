600fc34b516c66666a72d4a8518522c4e576f751
w-495 <w@w-495.ru>
2012-01-24 12:38:50 +0400

    my sql connection pool

---
+++ emysql/src/mysql_con_pool.erl
@@ -0,0 +1,30 @@
+
+-module(mysql_con_pool).
+
+-define(NUMBER, 10).
+
+-export([
+    start_link/5,
+    start_link/6,
+    test/0,
+    test/1
+]).
+
+start_link(PoolId, Host, User, Password, Database) ->
+    start_link(PoolId, Host, User, Password, Database, ?NUMBER).
+
+start_link(PoolId, Host, User, Password, Database, Number) ->
+    Link = mysql:start_link(PoolId,Host,User,Password,Database),
+    start_link_iter(PoolId,Host,User,Password,Database, Number),
+    Link.
+
+start_link_iter(_PoolId, _Host, _User, _Password, _Database, 0) -> ok;
+start_link_iter(PoolId, Host, User, Password, Database, Step) ->
+    mysql:connect(PoolId,Host,undefined,User,Password,Database,true),
+    start_link_iter(PoolId, Host, User, Password, Database, Step-1).
+
+test()->
+    ok.
+
+test(speed)->
+    ok.
