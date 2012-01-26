f2e5729b9ba807bd5da0f02658c0d741edad7ccd
w-495 <w@w-495.ru>
2012-01-26 13:58:52 +0400

    sdsd

---
+++ web/js/source/class/bsk/view/Form/AcvVideoCreateMaster/Upload.js
@@ -0,0 +1,213 @@
+/* ************************************************************************
+    https://gist.github.com/1639960
+    
+#asset(qx/icon/Tango/16/actions/document-save.png)
+************************************************************************ */
+
+qx.Class.define("bsk.view.Form.AcvVideoCreateMaster.Upload",
+{
+    extend : Object,
+    
+    construct : function(uReq) {
+        this.uReq = uReq;
+        this.buildForm();
+    },
+
+    members : {
+        
+        /* Upload request берется из конструктора */
+        uReq : null,
+        
+        /* Download request делаем сами*/
+        dReq : null,
+        
+        /**
+         * Download  request config
+         *
+         * Предполагается, что загружать данные каждая страница
+         * мастера будет самостоятельно, а вот выгружаться на сервер они будут
+         * одним запросом.
+         * 
+        **/
+        drc : {             
+            url: "",        // 
+            method: "",     // POST \ GET
+            mimetype: ""    // application/json
+        },
+        
+        urc : {  // upload request config
+            imgurl: "/update-adv-com/upload-video"
+        },
+        
+        getComposite : function(){
+            return this.composite;
+        },
+
+        /**
+         * Поля формы.
+         * Вообще, учитывая, богатсво форм они могут не понадобиться.
+        **/
+        inp : {
+            Url:        null,
+            Ref:        null,
+            Preroll:    null,
+            Midroll:    null,
+            Postroll:   null,
+            Pauseroll:  null
+        },
+        
+        buildForm : function(){
+            var RFM = bsk.view.Form.AbstractForm.REQUIRED_FIELD_MARKER;
+            
+            /* Сопровождающая картинка */
+            this.picButton = new bsk.view.Form.Upload.UploadButton("uploadfile", null, "icon/16/actions/document-save.png"),
+            this.picForm = new bsk.view.Form.Upload.UploadForm('uploadFrm', this.urc.imgurl);
+            
+            var layout = new qx.ui.layout.Grid(2, 5);
+            layout.setColumnFlex(1, 1);
+            layout.setColumnAlign(0, "right", "top");
+            
+            this.composite  = new qx.ui.container.Composite (layout);
+
+            this.inp.Name = new qx.ui.form.TextField();
+            this.inp.Url = new qx.ui.form.TextField();
+            this.inp.Ref = new qx.ui.form.TextField();
+            
+            var pageName = new qx.ui.basic.Label()
+                .set({
+                    value: "Загруска видео",  font: "bold",
+                    alignX: "left", rich : true
+                });
+                
+            var vertical_offset = -1;
+            this.composite.add(pageName, {row:++vertical_offset, column:0, colSpan:2})
+            
+            this.composite.add(new qx.ui.basic.Label().set({value: "Урл",  rich : true}),
+                    {row:++vertical_offset, column:0});
+            this.composite.add(this.inp.Ref,   {row:vertical_offset, column:1});
+            
+            this.composite.add(new qx.ui.basic.Label().set({value: "Файл",  rich : true}),
+                    {row:++vertical_offset, column:0});
+            this.composite.add(this._buildPicFormCnt(),   {row:vertical_offset, column:1});
+            
+            this.inp.Preroll = new qx.ui.form.CheckBox("Preroll")
+            this.inp.Midroll = new qx.ui.form.CheckBox("Midroll")
+            this.inp.Postroll = new qx.ui.form.CheckBox("Postroll")
+            this.inp.Pauseroll = new qx.ui.form.CheckBox("Pauseroll")
+            
+            var boxPlace = new qx.ui.groupbox.GroupBox("Размещение ролика");
+            boxPlace.setLayout(new qx.ui.layout.VBox(2));
+            boxPlace.add(this.inp.Preroll);
+            boxPlace.add(this.inp.Midroll);
+            boxPlace.add(this.inp.Postroll);
+            boxPlace.add(this.inp.Pauseroll);
+
+            this.composite.add(boxPlace,   {row:++vertical_offset, column:0,colSpan:2});
+            
+            return this.composite;
+        },
+        
+        /**
+            Обработчики событий,
+                которые не удалось вынести внутрь
+                    отдельных виджетов.
+        **/
+        addListeners: function() {            
+            var _this = this;
+            /* События виджетов для сопровождающей картикни  */
+            this.picButton.addListener('changeFileName',function(e){
+                if('' != e.getData()) {
+                    bsk.view.Form.Upload.UploadFakeStatusBar.on();
+                    
+                    _this.picForm.setParameter("prev", _this.inp.Url.getValue());
+                    _this.inp.Url.setValue(_this.picButton.getFileName());
+                    _this.picForm.send();    
+                }
+            });
+            this.picForm.addListener('completed',function(e) {
+                var response = _this.picForm.getIframeTextContent();
+                bsk.view.Form.Upload.UploadFakeStatusBar.off();
+                _this.inp.Url.setValue(response);
+            });
+        },
+        
+        /**
+            Создает область загрузки картинки.
+        **/
+        _buildPicFormCnt: function() {
+            var pic_layout = new qx.ui.layout.Grid(12, 6);
+            var picFormCnt = new qx.ui.container.Composite(pic_layout).set({
+                allowGrowX: true
+              });
+            if(!this.inp.Url)
+                return picFormCnt;
+            
+            pic_layout.setColumnFlex(0, 1);
+            pic_layout.setColumnAlign(0, "right", "middle");
+            picFormCnt.add(this.inp.Url,  {row:0, column:0});
+            this.picForm.setParameter('rm','upload');
+            this.picForm.setLayout(new qx.ui.layout.Basic);
+            picFormCnt.add(this.picForm, {row:0, column:1});
+            this.picForm.add(this.picButton , {left:0,top:0});
+            return picFormCnt;
+        },
+        
+        
+        /**
+            Получает данные с сервера.
+        **/
+        loadFormData : function(id, paramName) {
+            this.dReq = new qx.io.remote.Request
+                (this.drc.url, this.drc.method, this.drc.mimetype);
+            this.dReq.setTimeout(60000);
+            this.dReq.setParameter(paramName, id);
+            this.dReq.addListener("completed", this._onLoadFormDataCompl, this);
+            this.dReq.send();
+        },
+        
+        _onLoadFormDataCompl : function(response) {
+            var result = response.getContent();
+            if (false == bsk.util.errors.process(this, result))
+                return false;
+            this.fillForm(result);
+            return true;
+        },
+        
+        /**
+            Заполняет форму полученными данными.
+        **/
+        fillForm : function(data) {
+            for(var fieldName in this.inp){
+                var item = fieldName.toLowerCase();
+                this.inp[fieldName].setValue(data.value[item])
+            }
+        },
+        
+        /**
+            Проверяет коректность данных
+        **/
+        validateForm : function() {
+            var flag = true;
+            return flag;
+        },
+        
+        /**
+            Применив некоторые преобразования <<загружает>> данные на сервер
+        **/
+        saveData : function(e) {
+            var formIsValid = this.validateForm();
+            if(formIsValid){
+                var res = {}
+                for(var fieldName in this.inp){
+                    item = fieldName.toLowerCase()
+                    res[item] = this.inp[fieldName].getValue();
+                }  
+                for(var item in res){
+                    this.uReq.setParameter(item, res[item], true);
+                }
+            }
+            return formIsValid;
+        }
+    }
+});
+
---
+++ web/js/source/class/bsk/view/Form/AcvVideoCreateMaster/UsersTargeting.js
@@ -0,0 +1,211 @@
+/* ************************************************************************
+    https://gist.github.com/1639960
+************************************************************************ */
+
+qx.Class.define("bsk.view.Form.AcvVideoCreateMaster.UsersTargeting",
+{
+    extend : Object,
+    
+    construct : function(uReq) {
+        this.uReq = uReq;
+        this.buildForm();
+    },
+
+    members : {
+        
+        /* Upload request берется из конструктора */
+        uReq : null,
+        
+        /* Download request делаем сами*/
+        dReq : null,
+        
+        /**
+         * Download  request config
+         *
+         * Предполагается, что загружать данные каждая страница
+         * мастера будет самостоятельно, а вот выгружаться на сервер они будут
+         * одним запросом.
+         * 
+        **/
+        drc : {             
+            url: "",        // 
+            method: "",     // POST \ GET
+            mimetype: ""    // application/json
+        },
+        
+        getComposite : function(){
+            return this.composite;
+        },
+
+        /**
+         * Поля формы.
+         * Вообще, учитывая, богатсво форм они могут не понадобиться.
+        **/
+        inp : {
+            Age_from:null,
+            Age_to:null,
+            Time_from:null,
+            Time_to:null,
+            Gender:null
+        },
+        
+        buildForm : function(){
+            var RFM = bsk.view.Form.AbstractForm.REQUIRED_FIELD_MARKER;
+            var pageName = new qx.ui.basic.Label()
+                .set({
+                    value: "Таргетирование",  font: "bold",
+                    alignX: "left", rich : true
+                });
+                
+            var layout = new qx.ui.layout.Grid(1, 5);
+            layout.setColumnFlex(0, 1);
+            layout.setColumnAlign(0, "right", "top");
+            
+            this.composite  = new qx.ui.container.Composite (layout);
+            
+            var boxGender = this.makeBoxGender();
+            var boxAge = this.makeBoxAge();
+            var boxTime = this.makeBoxTime();
+            
+            var vertical_offset = -1;
+            
+            this.composite.add(pageName,
+                {row:++vertical_offset, column:0});
+            
+            this.composite.add(boxGender,
+                {row:++vertical_offset, column:0});
+            
+            this.composite.add(boxAge,
+                {row:++vertical_offset, column:0});
+            
+            this.composite.add(boxTime,
+                {row:++vertical_offset, column:0});
+            
+            return this.composite;
+        },
+        
+        makeBoxGender : function() {
+            this.inp.Gender = new qx.ui.form.SelectBox();
+            this.__fillSelect(this.inp.Gender, [
+                    {name:"Все", value: null},
+                    {name:"Мужской", value: "true"},
+                    {name:"Женский", value: "false"}
+                ], "name", "value");
+            var vertical_offset = 0;
+            var boxGender = new qx.ui.groupbox.CheckGroupBox("Пол");
+            var layout2 = new qx.ui.layout.Grid(1, 1)
+            boxGender.setLayout(layout2);
+            boxGender.setValue(false);
+            layout2.setColumnFlex(0, 1);
+            boxGender.add(this.inp.Gender, {row:0, column:0});
+            return boxGender;
+        },
+        
+        makeBoxAge : function() {
+            this.inp.Age_from = new qx.ui.form.Spinner(1, 1, 100);
+            this.inp.Age_to = new qx.ui.form.Spinner(2, 2, 100);
+            var vertical_offset = 0;
+            var boxAge = new qx.ui.groupbox.CheckGroupBox("Возраст");
+            var layout2 = new qx.ui.layout.Grid(1, 2)
+            boxAge.setLayout(layout2);
+            boxAge.setValue(false);
+            layout2.setColumnFlex(1, 1);
+            boxAge.add(new qx.ui.basic.Label().set({value: "От:",  rich : true}), {row:++vertical_offset, column:0});
+            boxAge.add(this.inp.Age_from, {row:vertical_offset, column:1});
+            boxAge.add(new qx.ui.basic.Label().set({value: "До:",  rich : true}), {row:++vertical_offset, column:0});
+            boxAge.add(this.inp.Age_to, {row:vertical_offset, column:1});
+            return boxAge;
+        },
+
+        makeBoxTime : function() {
+            this.inp.Time_from = new qx.ui.form.Spinner(0, 0, 24);
+            this.inp.Time_to = new qx.ui.form.Spinner(0, 24, 24);
+            var vertical_offset = 0;
+            var boxTime = new qx.ui.groupbox.CheckGroupBox("Время показа");
+            var layout3 = new qx.ui.layout.Grid(1, 2);
+            layout3.setColumnFlex(1, 1);
+            boxTime.setLayout(layout3);
+            boxTime.setValue(false);
+            boxTime.add(new qx.ui.basic.Label().set({value: "От:",  rich : true}), {row:++vertical_offset, column:0});
+            boxTime.add(this.inp.Time_from, {row:vertical_offset, column:1});
+            boxTime.add(new qx.ui.basic.Label().set({value: "До:",  rich : true}), {row:++vertical_offset, column:0});
+            boxTime.add(this.inp.Time_to, {row:vertical_offset, column:1});
+            return boxTime;
+        },
+        
+        /**
+            Получает данные с сервера.
+        **/
+        loadFormData : function(id, paramName) {
+            this.dReq = new qx.io.remote.Request
+                (this.drc.url, this.drc.method, this.drc.mimetype);
+            this.dReq.setTimeout(60000);
+            this.dReq.setParameter(paramName, id);
+            this.dReq.addListener("completed", this._onLoadFormDataCompl, this);
+            this.dReq.send();
+        },
+        
+        __fillSelect : function(sel, vals, alias, value) {
+            sel.itemMap = [];
+            for(var j=0; j<vals.length; j++) {
+                var SI = vals[j];
+                var selItem = new qx.ui.form.ListItem(SI[alias], null, SI[value]);
+                sel.itemMap[SI[value]] = selItem;
+                sel.add(selItem);
+            }
+        },
+        
+        _onLoadFormDataCompl : function(response) {
+            var result = response.getContent();
+            if (false == bsk.util.errors.process(this, result))
+                return false;
+            this.fillForm(result);
+            return true;
+        },
+        
+        /**
+            Заполняет форму полученными данными.
+        **/
+        fillForm : function(data) {
+            for(var fieldName in this.inp){
+                var item = fieldName.toLowerCase();
+                this.inp[fieldName].setValue(data.value[item])
+            }
+        },
+        
+        /**
+            Проверяет коректность данных
+        **/
+        validateForm : function() {
+            var flag = true;
+            return flag;
+        },
+        
+        /**
+            Применив некоторые преобразования <<загружает>> данные на сервер
+        **/
+        saveData : function(e) {
+            var formIsValid = this.validateForm();
+            if(formIsValid){
+                var res = {}
+                
+                console.log(this.inp.Gender.getSelection()[0].getModel());
+                
+                for(var fieldName in this.inp){
+                    item = fieldName.toLowerCase();
+                    if("gender" == item){
+                        res.gender = this.inp.Gender.getSelection()[0].getModel();
+                    }
+                    else{
+                        res[item] = this.inp[fieldName].getValue();
+                    }
+                }  
+                for(var item in res){
+                    this.uReq.setParameter(item, res[item], true);
+                }
+            }
+            return formIsValid;
+        }
+    }
+});
+
