36dfcfcec9c9c901ae859fb07505c33f255803fa
w-495 <w@w-495.ru>
2012-01-30 12:20:48 +0400

    base

--- web/js/source/class/bsk/view/SortedSelListTree.js
+++ web/js/source/class/bsk/view/SortedSelListTree.js
@@ -52,40 +52,37 @@ qx.Class.define("bsk.view.SortedSelListTree",
         },
         
         testItems : function(pattern) {
-            this.pattern = RegExp(pattern);
+            this.pattern = RegExp("^" + pattern );
             //this.data = bsk.util.utils.clone(this.shadow_data);
-            
-            console.log(this.data);
             
             for(var key in this.shadow_data){
                 var checkbox = this.shadow_data[key];
                 var value = checkbox.bsk_element;
                 var name = value.name;
                 var description = value.description;
-                
-                console.log("this.data[value.id] = ", this.data[value.id]);
-                if(!this.data[key]){
-                    var Item = new qx.ui.tree.TreeFile();
-                    var checkbox = new qx.ui.form.CheckBox();
-                    checkbox.setFocusable(false);
-                    checkbox.bsk_element = value;
-                    checkbox.Item = Item;
-                    Item.setIcon(null);
-                    Item.addWidget(checkbox);
-                    Item.addLabel("" + value[this.labelFieldName]);
-                    Item.addWidget(new qx.ui.core.Spacer(), {flex: 1});
-                    var text = new qx.ui.basic.Label(value[this.descrFieldName]);
-                    text.setWidth(150);
-                    Item.addWidget(text);
-                    this.root.add(Item);
-                    this.data[value.id] = checkbox;
-                }
                 // checkbox.show();
                 // checkbox.Item.show();
                 if(!this.pattern.test(name)&&!this.pattern.test(description)){
                     this.remItem(value);
                     // checkbox.hide();
                     // checkbox.Item.hide();
+                }else{
+                    if(!this.data[key]){
+                        var Item = new qx.ui.tree.TreeFile();
+                        var checkbox = new qx.ui.form.CheckBox();
+                        checkbox.setFocusable(false);
+                        checkbox.bsk_element = value;
+                        checkbox.Item = Item;
+                        Item.setIcon(null);
+                        Item.addWidget(checkbox);
+                        Item.addLabel("" + value[this.labelFieldName]);
+                        Item.addWidget(new qx.ui.core.Spacer(), {flex: 1});
+                        var text = new qx.ui.basic.Label(value[this.descrFieldName]);
+                        text.setWidth(150);
+                        Item.addWidget(text);
+                        this.root.add(Item);
+                        this.data[value.id] = checkbox;
+                    }
                 }
             }
         },
