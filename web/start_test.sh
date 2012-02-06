1f6556bbb932006d403f9cdf0c1f76cdcaa2b6a8
w-495 <w@w-495.ru>
2012-02-06 13:58:46 +0400

    ch

--- web/start_test.sh
+++ web/start_test.sh
@@ -3,7 +3,7 @@
     PATH=$PATH:/usr/local/lib/
     export PATH
 
-    NODENAME="advsystem@192.168.2.156"
+    NODENAME="advsystem@localhost"
     CONFIG=app_dev
 
     ERL_ARGS="+K true +A 128 +P 1000000"
