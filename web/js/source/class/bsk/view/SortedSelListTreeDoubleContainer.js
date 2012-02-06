7c948266ca37e5f2287c485937d5a709e21e0b09
w-495 <w@w-495.ru>
2012-02-06 13:42:12 +0400

    all

---
+++ web/js/source/class/bsk/view/SortedSelListTreeDoubleContainer.js
@@ -0,0 +1,43 @@
+/* ************************************************************************
+************************************************************************ */
+
+qx.Class.define("bsk.view.SortedSelListTreeDoubleContainer",
+{
+    extend : qx.ui.container.Composite,
+    
+    /**
+     *
+     *  @param url --- URL запроса.
+     *  @param labelFieldName
+     *  @param descrFieldName
+     *  @param paramdict --- список параметоров url (может быть undefined)
+     */
+    construct : function(url, labelFieldName, descrFieldName, paramdict) {
+        this.base(arguments);
+        
+        this.field = new qx.ui.form.TextField()
+            .set({placeholder: "Фраза для поиска в списке"});
+        this.tree = new bsk.view.SortedSelListTree(this, url, labelFieldName, descrFieldName, paramdict)
+        
+        var vbox = new qx.ui.layout.VBox();
+        this.setLayout(vbox);
+        this.add(this.field);
+        this.add(this.tree);
+        
+        this.field.addListener("input", this._OnChange, this);
+    },
+
+    members : {
+        
+        _OnChange : function(e){
+            console.log("getData = ", e.getData());
+            this.tree.testItems(e.getData());
+        },
+        
+        getSelectedId : function() {
+            this.tree.getSelectedId();
+        }
+    }
+});
+
+
