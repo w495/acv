10f98fbbb89482d2f2c7dd7cf2831d86542c84c5
Serge Kozhevnikov <cff@sir.(none)>
2012-02-17 10:38:33 +0400

    sdg

--- web/src/ctl/index.erl
+++ web/src/ctl/index.erl
@@ -6,10 +6,10 @@
 
 -compile(export_all).
 
-%call_before(Req) ->
-%    try authorization:auth_required(Req)
-%        catch throw:auth_required -> throw({redirect, "/login", []})
-%    end.
+call_before(Req) ->
+    try authorization:auth_required(Req)
+        catch throw:auth_required -> throw({redirect, "/login", []})
+    end.
 
 index(_Req) ->
     throw({serve_static, "index.html", ?RIA_HOME, [{"Cache-Control", "no-cache "}]}).
