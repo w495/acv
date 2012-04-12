aacc59adb61a26eafe56396b82f88943ee7bca3b
w-495 <w@w-495.ru>
2012-04-12 20:50:11 +0400

    ++

--- web/js/source/class/zqr/view/Form/AcvVideoCreateMaster/Upload.js
+++ web/js/source/class/zqr/view/Form/AcvVideoCreateMaster/Upload.js
@@ -216,9 +216,9 @@ qx.Class.define("zqr.view.Form.AcvVideoCreateMaster.Upload",
         validateForm : function() {
             var flag = true;
             
-            flag &= zqr.view.Form.AbstractForm.customFormChkLength(3, 50, this.inp.Link_title);
-            flag &= zqr.view.Form.AbstractForm.customFormChkLength(3, 50, this.inp.Url);
-            flag &= zqr.view.Form.AbstractForm.customFormChkLength(5, 50, this.inp.Ref);
+            flag &= zqr.view.Form.AbstractForm.customFormChkLength(3, 5000, this.inp.Link_title);
+            flag &= zqr.view.Form.AbstractForm.customFormChkLength(3, 5000, this.inp.Url);
+            flag &= zqr.view.Form.AbstractForm.customFormChkLength(5, 5000, this.inp.Ref);
             flag &= zqr.view.Form.AbstractForm.customFormChkSymb(this.inp.Link_title);
             //flag &= zqr.view.Form.AbstractForm.customFormChkUrl(this.inp.Url);;
             flag &= zqr.view.Form.AbstractForm.customFormChkVideoFileName(this.inp.Ref);
