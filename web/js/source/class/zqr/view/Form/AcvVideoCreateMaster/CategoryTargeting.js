d05458bc425fb848647319025775fce3864134fa
cff <cff@sir.(none)>
2012-04-16 15:20:11 +0400

    таргетирование по жанрам, вставил чекбокс

--- web/js/source/class/zqr/view/Form/AcvVideoCreateMaster/CategoryTargeting.js
+++ web/js/source/class/zqr/view/Form/AcvVideoCreateMaster/CategoryTargeting.js
@@ -40,21 +40,28 @@ qx.Class.define("zqr.view.Form.AcvVideoCreateMaster.CategoryTargeting",
         },
         
         getComposite : function(){
-            return this.composite;
+            //return this.composite;
+            return this.boxCat;
         },
         
         buildForm : function(){
             var RFM = zqr.view.Form.AbstractForm.REQUIRED_FIELD_MARKER;
-            var pageName = new qx.ui.basic.Label()
+/*            var pageName = new qx.ui.basic.Label()
                 .set({
                     value: "Таргетирование по категориям",  font: "bold",
                     alignX: "left", rich : true
-                });
+                }); */
+
+
+            this.boxCat = new qx.ui.groupbox.CheckGroupBox("Таргетирование по жанрам");
+            this.boxCat.setValue(false);
+
             var layout = new qx.ui.layout.Grid(2, 1);
             layout.setColumnFlex(0, 1);
             layout.setColumnAlign(0, "right", "top");
             
-            this.composite  = new qx.ui.container.Composite (layout);
+            //this.composite  = new qx.ui.container.Composite (layout);
+            this.boxCat.setLayout(layout);
             
             this.inp.List = new zqr.view.
                 Sltdac(
@@ -66,12 +73,12 @@ qx.Class.define("zqr.view.Form.AcvVideoCreateMaster.CategoryTargeting",
             
             var vertical_offset = -1;
             
-            this.composite.add(pageName,
-                {row:++vertical_offset, column:0});
-            this.composite.add(this.inp.List,
+//            this.boxCat.add(pageName,
+//                {row:++vertical_offset, column:0});
+            this.boxCat.add(this.inp.List,
                 {row:++vertical_offset, column:0});
             
-            return this.composite;
+            return this.boxCat;
         },
         
         
@@ -88,6 +95,7 @@ qx.Class.define("zqr.view.Form.AcvVideoCreateMaster.CategoryTargeting",
             }
             console.log("list = ", list);
             this.inp.List.setChecked(list);
+            this.boxCat.setValue(list.length > 0);
         },
         
         /**
@@ -102,12 +110,12 @@ qx.Class.define("zqr.view.Form.AcvVideoCreateMaster.CategoryTargeting",
             Применив некоторые преобразования <<загружает>> данные на сервер
         **/
         saveData : function(e) {
-            var list = this.inp.List.getSelectedId();
-  
             if(this.validateForm()) {
+                var list = [];
+                if(this.boxCat.getValue())
+                    list = this.inp.List.getSelectedId();
                 this.uReq.setParameter("cat_list", list, true);
             }
-            
             return true;
         }
     }
